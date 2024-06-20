f120cd33093bbfdf0fb7ae56d9441448778c66b5
vasusumeet <vasuseth1305@gmail.com>
2024-06-20 23:01:38 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,33 +1,33 @@
-import express, { request } from "express";
-import {PORT, mongoDBURL} from "./config.js";
-import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
-import cors from "cors";
-const app=express();
+import cors from 'cors';
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(request,response)=>{
-    console.log(request);
-    return response.status(234).send('Welcome to MERN Tutorial');
-})
-app.use('/books',booksRoute);
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome to MERN Tutorial');
+});
 
-
-
+app.use('/books', booksRoute);
 
 mongoose
- .connect(mongoDBURL)
- .then(()=>{
+  .connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
     console.log('App is connected to Database');
-    app.listen(PORT, ()=>{
-        console.log(`App is listening to port: ${PORT}`);
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
     });
- })
- .catch((error)=>{
+  })
+  .catch((error) => {
     console.log(error);
+  });
 
- })
 export default app;
+
