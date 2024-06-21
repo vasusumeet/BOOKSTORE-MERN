a0dfded22f3c07a541614731bddef952377ec24d
vasusumeet <vasuseth1305@gmail.com>
2024-06-21 11:24:37 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -4,4 +4,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 5555;
-export const mongoDBURL = mongodb+srv://vasu:vasu@book-store-mern.ijsy4ja.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-store-MERN;
+export const mongoDBURL = process.env.mongoDBURL;
