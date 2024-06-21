1403599e7622e3fb7bf32ba4ab88e07776ab2008
vasusumeet <vasuseth1305@gmail.com>
2024-06-21 11:27:34 +0530

    Update CreateBooks.jsx

--- Frontend/src/pages/CreateBooks.jsx
+++ Frontend/src/pages/CreateBooks.jsx
@@ -20,7 +20,7 @@ const CreateBooks = () => {
       publishYear
     };
     setLoading(true);
-    axios.post('http://localhost:5555/books',data)
+    axios.post('https://bookstore-mern-seven.vercel.app/books',data)
     .then(()=>{
       setLoading(false);
       enqueueSnackbar('Book Created Successfully.',{variant:'success'})
