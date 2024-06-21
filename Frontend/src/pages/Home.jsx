3a34297081a2dccfbcdfa69faa1862c552f6d439
vasusumeet <vasuseth1305@gmail.com>
2024-06-21 11:28:32 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books')
+      .get('https://bookstore-mern-seven.vercel.app/books')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
